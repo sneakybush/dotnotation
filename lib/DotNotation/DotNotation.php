0151c57403c337288432848fd2d6a91684d184cb
CrazyCoder <true.isnt.false@gmail.com>
2013-12-19 17:46:21 +0700

    wrote some code to make the concept clear

--- lib/DotNotation/DotNotation.php
+++ lib/DotNotation/DotNotation.php
@@ -16,6 +16,9 @@ class DotNotation implements ArrayAccess
     // all data obtained is stored here
     private $_data = [];
     
+    // prevents the data from being changed externally
+    private $_readOnly;
+    
     // supported data types 
     // avoid 1 & 0 in constants, variables and such things
     const PHP_SERIALIZED = 2;    
@@ -24,22 +27,36 @@ class DotNotation implements ArrayAccess
     
     public function offsetExists ($offset)
     {
+        // not so beautiful, thinking how to make it better 
+        try
+        {
+            $this->get ($offset);
+        }
+        catch (InvalidArgumentException $exception)
+        {
+            return false;
+        }
+        catch (UnexpectedValueException $exception)
+        {
+            return false;
+        }
         
+        return true;
     }
 
     public function offsetGet ($offset)
     {
-        
+        return $this->get ($offset);
     }
 
     public function offsetSet ($offset, $value)
     {
-        
+        $this->set ($offset, $value);
     }
 
     public function offsetUnset ($offset)
     {
-        
+        $this->remove ($offset);
     }
 
     public function root ()
@@ -101,6 +118,23 @@ class DotNotation implements ArrayAccess
         }
         
         return $pointer;
+    }
+    
+    // you should only use THIS METHOD, not the one above
+    public function get ($path)
+    {
+        $path = $this->_parsePath ($path);
+        return $this->_get ($this->_data , $path);
+    }
+    
+    public function set ($path, $value)
+    {
+        
+    }
+    
+    public function remove ($path)
+    {
+        
     }
 }
 
